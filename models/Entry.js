e7ec8d4cb0765c9ea792d855f1ad8025bf479735
Ali <ali.kamel5958@gmail.com>
2026-09-19 04:36:16 +0300

    reached part 6

---
+++ models/Entry.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const entrySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100,
+    },
+    entryBody: {
+        type: String,
+        maxLength: 350
+    },
+    isPublic: {
+        type: Boolean,
+        default: false,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+module.exports = Entry
---
+++ routes/entries.routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const isSignedIn = require('../middleware/is-signed-in');
+const Entry = require('../models/Entry');
+
+// Part 3 - New Entry Page
+router.get('/new', isSignedIn, (req, res) => {
+    res.render('new-journal.ejs');
+});
+
+// Part 4 - Create an Entry
+router.post('/', isSignedIn, async (req, res) => {
+    console.log(req.session.user);
+    await Entry.create({
+        title: req.body.title,
+        entryBody: req.body.entryBody,
+        isPublic: Boolean(req.body.isPublic),
+        owner: req.session.user._id
+    });
+    res.redirect('/entries');
+});
+
+// Part 5 - Public Entries
+router.get('/', async (req, res) => {
+    const foundEntries = await Entry.find({ isPublic: true });
+    res.render('all-entries.ejs', { entries: foundEntries });
+});
+
+module.exports = router;
